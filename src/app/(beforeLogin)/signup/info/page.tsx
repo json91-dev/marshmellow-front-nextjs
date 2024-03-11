4bdd5286f393297ae2c0c9b9f856735ab9334112
jungwoole91 <jungwoole91@gmail.com>
2024-03-12 03:34:43 +0900

    [feat] 지원서 사진 등록 UI 작업

--- src/app/(beforeLogin)/signup/info/page.tsx
+++ src/app/(beforeLogin)/signup/info/page.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import ReactDatePicker from '@/app/(beforeLogin)/signup/info/_components/ReactDatePicker';
 import HorizontalLine from '@/app/(beforeLogin)/signup/info/_components/HorizontalLine';
+import Image from 'next/image';
 
 /** 회원가입 정보 입력 페이지 **/
 export default function Info() {
@@ -15,47 +16,63 @@ export default function Info() {
       <div className={style.description}>
         입사 지원서는 순서대로 기재 바랍니다. '최종제출' 버튼을 눌러야 입사 지원이 완료되오니 유의하시기 바랍니다.
       </div>
+
       <div className={style.secitionInfo}>
         <div>*기본정보</div>
         <HorizontalLine />
       </div>
-      <div>
-        <div>이미지 영역</div>
-        <div className={style.label}>
+
+      <div className={style.imageArea}>
+        <div className={style.image}>
+          <Image src="/images/icon_airpod.png" alt="No Image" fill objectFit="contain" />
+        </div>
+        <div className={style.info}>
           <div className={style.label}>지원서 사진등록</div>
           <div className={style.label}>권장사이즈: 가로 160px X 세로 160px)</div>
         </div>
       </div>
-      <div className={style.name}>
+
+      <div className={style.nameArea}>
         <div className={style.label}>이름</div>
         <input className={style.textInput} type="text" placeholder="아이디를 입력해주세요" />
       </div>
-      <div className={style.nickname}>
+
+      <div className={style.nicknameArea}>
         <div className={style.label}>닉네임</div>
         <div className={style.nicknam}>
           <input className={style.textInput} type="text" placeholder="특수문자 제외 2~8자" />
           <div>중복 확인</div>
         </div>
       </div>
-      <div>성별</div>
-      <div>토글 성별영역</div>
-      <div>생년월일</div>
-      <ReactDatePicker />
+
+      <div className={style.genderArea}>
+        <div>성별</div>
+        <div>토글 성별영역</div>
+      </div>
+
+      <div className={style.birthArea}>
+        <div>생년월일</div>
+        <ReactDatePicker />
+      </div>
 
       {/*기타*/}
       <div className={style.secitionInfo}>
         <div>*기타</div>
         <HorizontalLine />
       </div>
-      <div>
+
+      <div className={style.contactArea}>
         <div>지원경로</div>
         <div>선택영역</div>
       </div>
-      <div>
-        <div>추천인 입력</div>
-        <div>추천인 입력시, 마시멜로우 10개를 드려요</div>
+
+      <div className={style.recommendArea}>
+        <div>
+          <div>추천인 입력</div>
+          <div>추천인 입력시, 마시멜로우 10개를 드려요</div>
+        </div>
+        <div>추천인 닉네임을 입력해주세요.</div>
       </div>
-      <div>추천인 닉네임을 입력해주세요.</div>
 
       <div>다음 단계 버튼</div>
     </div>
