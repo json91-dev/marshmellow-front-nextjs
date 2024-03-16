9cd1352c12c6c5fd943daedfd4010c4ff1604a4e
jungwoole91 <jungwoole91@gmail.com>
2024-03-16 15:45:07 +0900

    [feat] 회원가입 본인인증 화면 UI 작업

--- src/app/(beforeLogin)/signup/identify/page.tsx
+++ src/app/(beforeLogin)/signup/identify/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import SectionInfo from '@/app/(beforeLogin)/signup/_components/SectionInfo';
 import ConfirmButton from '@/app/(beforeLogin)/signup/_components/ConfirmButton';
 import Image from 'next/image';
+import IdentifyCheck from '@/app/(beforeLogin)/signup/identify/_components/IdentifyCheck';
 
 /** 회원가입 인증 페이지  **/
 export default function Identify() {
@@ -11,13 +12,7 @@ export default function Identify() {
     <div className={style.container}>
       <InformationTab index={1} />
       <SectionInfo title={'*본인인증'} />
-      <div className={style.grayArea}>
-        <div className={style.image}>
-          <Image src="/images/icon_airpod_2.png" alt="No Image" fill objectFit="contain" />
-        </div>
-        <div className={style.description}>입사지원을 위해 최초 1회 본인인증이 필요합니다.</div>
-        <ConfirmButton text={'본인인증하기'} customStyle={{ fontSize: '1.3rem', width: '80%' }} />
-      </div>
+      <IdentifyCheck />
     </div>
   );
 }
