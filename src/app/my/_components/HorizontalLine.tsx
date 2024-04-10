38f8fa4c3a8528809fcd1ffc93a1c60f4b9c9b79
jungwoole91 <jungwoole91@gmail.com>
2024-04-10 19:38:07 +0900

    [feat] 내 마시멜로우 페이지 기본 UI 작업

--- src/app/my/_components/HorizontalLine.tsx
+++ src/app/my/_components/HorizontalLine.tsx
@@ -3,12 +3,13 @@ import { CSSProperties } from 'react';
 type Props = {
   color?: string;
   height?: string;
+  width?: string;
   customStyle?: CSSProperties;
 };
 
-const HorizontalLine = ({ color = 'black', height = '1px', customStyle = {} }) => {
+const HorizontalLine = ({ color = 'black', height = '1px', customStyle = {}, width = '100%' }: Props) => {
   const style = {
-    width: '100%',
+    width: width,
     border: `${height} solid ${color}`,
     ...customStyle,
   };
