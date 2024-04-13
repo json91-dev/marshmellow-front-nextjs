d5a0407afc6a60e3a0d152d302f576bb952c3680
jungwoole91 <jungwoole91@gmail.com>
2024-04-14 04:32:47 +0900

    [feat] 배송지 추가 메인 화면 UI 작업

--- src/app/my/address/add/page.tsx
+++ src/app/my/address/add/page.tsx
@@ -1,3 +1,54 @@
 export default function AddressAddPage() {
-  return <></>;
+  return (
+    <div>
+      <div>
+        <label htmlFor={'locationName'}>*배송지명</label>
+        <input id={'locationName'} type="text" />
+      </div>
+
+      <div>
+        <label htmlFor={'name'}>*이름</label>
+        <input id={'name'} type="text" />
+      </div>
+
+      <div>
+        <label htmlFor={'address'}>*주소</label>
+        <div>
+          <input id={'address'} type="text" disabled={true} />
+          <button>주소 검색</button>
+        </div>
+        <input id={'addressDetail'} type="text" disabled={true} />
+        <input id={'addressDetail2'} type="text" disabled={true} />
+      </div>
+
+      <div>
+        <label htmlFor={'name'}>*연락처1</label>
+        <div>
+          <input id={'number1'} type="number" />
+          <div>-</div>
+          <input id={'number2'} type="number" />
+          <div>-</div>
+          <input id={'number3'} type="number" />
+        </div>
+      </div>
+
+      <div>
+        <label htmlFor={'name'}>연락처2</label>
+        <div>
+          <input id={'number1'} type="number" />
+          <div>-</div>
+          <input id={'number2'} type="number" />
+          <div>-</div>
+          <input id={'number3'} type="number" />
+        </div>
+      </div>
+
+      <div>
+        <label htmlFor={'name'}>요청사항</label>
+        <input id={'name'} type="text" />
+      </div>
+
+      <button>확인</button>
+    </div>
+  );
 }
--- src/app/my/address/page.tsx
+++ src/app/my/address/page.tsx
@@ -1,10 +1,99 @@
 import style from './address.module.scss';
 import TopNavigation from '@/app/_components/common/TopNavigation';
+import Image from 'next/image';
 
 export default function AddressPage() {
   return (
     <div className={style.addressPage}>
       <TopNavigation title={'배송지 관리'} />
+      <div className={style.contentBody}>
+        <div className={style.addressCard}>
+          <div className={style.rightTopArea}>
+            <div className={style.mainAddressTag}>대표배송지</div>
+            {/*<button className={style.deleteAddressButton}>aa</button>*/}
+          </div>
+
+          <div className={style.name}>
+            <p>우리집</p>
+            <p>김이름</p>
+          </div>
+
+          <div className={style.addressArea}>
+            <p className={style.address}>서울특별시 서대문구 도로동 00-00</p>
+            <p className={style.detail}>상세 주소를 여기서 보여줍니다요. 사는 곳 동, 호수도</p>
+            <p className={style.phone}>010-0000-0000</p>
+            <p className={style.require}>요청사항 : 집 앞에다 두고가주세요</p>
+          </div>
+          <button className={style.editButton}>수정하기</button>
+        </div>
+
+        <div className={style.addressCard}>
+          <div className={style.rightTopArea}>
+            <button className={style.deleteAddressButton}>
+              <Image src={'/images/x.cancel.svg'} width={20} height={20} alt="No Image" />
+            </button>
+          </div>
+
+          <div className={style.name}>
+            <p>우리집</p>
+            <p>김이름</p>
+          </div>
+
+          <div className={style.addressArea}>
+            <p className={style.address}>서울특별시 서대문구 도로동 00-00</p>
+            <p className={style.detail}>상세 주소를 여기서 보여줍니다요. 사는 곳 동, 호수도</p>
+            <p className={style.phone}>010-0000-0000</p>
+            <p className={style.require}>요청사항 : 집 앞에다 두고가주세요</p>
+          </div>
+          <button className={style.editButton}>수정하기</button>
+        </div>
+
+        <div className={style.addressCard}>
+          <div className={style.rightTopArea}>
+            <button className={style.deleteAddressButton}>
+              <Image src={'/images/x.cancel.svg'} width={20} height={20} alt="No Image" />
+            </button>
+          </div>
+
+          <div className={style.name}>
+            <p>우리집</p>
+            <p>김이름</p>
+          </div>
+
+          <div className={style.addressArea}>
+            <p className={style.address}>서울특별시 서대문구 도로동 00-00</p>
+            <p className={style.detail}>상세 주소를 여기서 보여줍니다요. 사는 곳 동, 호수도</p>
+            <p className={style.phone}>010-0000-0000</p>
+            <p className={style.require}>요청사항 : 집 앞에다 두고가주세요</p>
+          </div>
+          <button className={style.editButton}>수정하기</button>
+        </div>
+
+        <div className={style.addressCard}>
+          <div className={style.rightTopArea}>
+            <button className={style.deleteAddressButton}>
+              <Image src={'/images/x.cancel.svg'} width={20} height={20} alt="No Image" />
+            </button>
+          </div>
+
+          <div className={style.name}>
+            <p>우리집</p>
+            <p>김이름</p>
+          </div>
+
+          <div className={style.addressArea}>
+            <p className={style.address}>서울특별시 서대문구 도로동 00-00</p>
+            <p className={style.detail}>상세 주소를 여기서 보여줍니다요. 사는 곳 동, 호수도</p>
+            <p className={style.phone}>010-0000-0000</p>
+            <p className={style.require}>요청사항 : 집 앞에다 두고가주세요</p>
+          </div>
+          <button className={style.editButton}>수정하기</button>
+        </div>
+
+        <button className={style.addButton}>
+          <p>배송지 추가하기 +</p>
+        </button>
+      </div>
     </div>
   );
 }
