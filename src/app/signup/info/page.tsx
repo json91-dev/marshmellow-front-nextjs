c9dcbbda5aa9332f9019fe1ea404c2abf1650214
jungwoole91 <jungwoole91@gmail.com>
2024-04-22 01:08:41 +0900

    [feat] react-hooks-form을 이용한 프로필 미리보기 구현

--- src/app/signup/info/page.tsx
+++ src/app/signup/info/page.tsx
@@ -11,6 +11,12 @@ import { useRouter } from 'next/navigation';
 // import { useForm } from 'react-hook-form';
 import { useSession } from 'next-auth/react';
 import TopNavigation from '@/app/_components/common/TopNavigation';
+import { useForm } from 'react-hook-form';
+
+interface Inputs {
+  imageFile: FileList;
+  image: FileList;
+}
 
 /** 회원가입 정보 입력 페이지 **/
 export default function SignupInfoPage() {
@@ -24,46 +30,31 @@ export default function SignupInfoPage() {
   const router = useRouter();
   const { data: session } = useSession();
 
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    setValue,
+  } = useForm<Inputs>();
+
+  const profile = watch('image');
+  const [profilePreviewUrl, setProfilePreviewUrl] = useState('');
+
+  useEffect(() => {
+    if (profile && profile.length > 0) {
+      const file = profile[0];
+      setProfilePreviewUrl(URL.createObjectURL(file));
+    }
+  }, [profile]);
+
+  /** 생년월일 선택 **/
   const selectGender = useCallback(
     (gender: string) => {
       setGender(gender);
     },
     [gender],
   );
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // setSubmitted(true);
-
-    if (!nickname) {
-      console.log('닉네임을 입력해주세요.');
-      return; // 닉네임이 입력되지 않았을 경우 함수를 여기서 종료
-    }
-
-    if (!gender) {
-      console.log('성별을 선택해주세요.');
-      return; // 성별이 선택되지 않았을 경우 함수를 여기서 종료
-    }
-
-    // 닉네임과 성별이 모두 입력되었을 때 처리할 로직
-    console.log('Submitted data:', { nickname, gender });
-  };
-
-  useEffect(() => {
-    // setBirthdate('1991.09.11');
-  }, []);
-
-  // const onDateTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   let { value } = e.target;
-  //   value = value.replace(/[^0-9]/g, ''); // 숫자가 아닌 문자는 제거
-  //   if (value.length > 4) {
-  //     value = value.slice(0, 4) + '.' + value.slice(4);
-  //   }
-  //   if (value.length > 7) {
-  //     value = value.slice(0, 7) + '.' + value.slice(7);
-  //   }
-  //   setBirthdate(value);
-  // };
 
   return (
     <div className={style.signupInfoPage}>
@@ -73,10 +64,14 @@ export default function SignupInfoPage() {
         <InformationTab index={2} />
         <SectionInfo title={'*기본정보'} />
 
-        <form className={style.form} onSubmit={handleSubmit}>
+        <form className={style.form}>
           <div className={style.imageArea}>
-            <div className={style.image}>
-              <Image src="/images/mallow.image.svg" alt="No Image" fill objectFit="contain" />
+            <input type="file" id="profileInput" style={{ display: 'none' }} accept="image/*" {...register('image')} />
+            <div className={style.image} onClick={() => document.getElementById('profileInput')?.click()}>
+              <>
+                {!profilePreviewUrl && <Image src="/images/mallow.image.svg" alt="No Image" fill objectFit="contain" />}
+                {profilePreviewUrl && <Image src={profilePreviewUrl} alt="No Image" fill objectFit="contain" />}
+              </>
             </div>
             <div className={style.info}>
               <div className={style.label}>지원서 사진등록</div>
