15f24b89a2120db44b7b770df92005e9f9af12f9
jungwoole91 <jungwoole91@gmail.com>
2024-07-20 02:44:35 +0900

    [feat] 럭키드로우 상품 당첨 페이지 UI 작업

--- src/app/recreation/luckydraw/winner/prize/page.tsx
+++ src/app/recreation/luckydraw/winner/prize/page.tsx
@@ -5,20 +5,25 @@ import { useRouter } from 'next/navigation';
 import Confetti from 'react-confetti';
 import React, { useEffect, useRef, useState } from 'react';
 import cx from 'classnames';
+import { useToastStore } from '@/store/toast';
+
 export default function LuckyDrawWinnerMarshmallow() {
   const router = useRouter();
   const mallowPageRef = useRef<HTMLDivElement>(null!);
   const [mallowPageClientWidthHeight, setMallowPageClientWidthHeight] = useState<{ width: number; height: number }>(null!);
-  const randomPrize = Math.floor(Math.random() * 3 + 1);
+  const randomPrize = useRef<number>(null!);
+  const { openToast } = useToastStore();
 
   useEffect(() => {
     setMallowPageClientWidthHeight({
       width: mallowPageRef.current?.clientWidth,
       height: mallowPageRef.current?.clientHeight,
     });
+
+    randomPrize.current = Math.floor(Math.random() * 3 + 1);
   }, []);
   return (
-    <div ref={mallowPageRef} className={style.winnerPrizePage}>
+    <div ref={mallowPageRef} className={cx(style.winnerPrizePage, randomPrize.current === 1 && style.fistPrize)}>
       <div className={style.confetti}>
         {mallowPageClientWidthHeight && (
           <Confetti
@@ -33,13 +38,24 @@ export default function LuckyDrawWinnerMarshmallow() {
         <div className={style.warning}>
           <p>{'0000.00.00 까지 수령정보를 입력해주세요.\n기간 내 정보 미제출 시 경품수령이 불가합니다.'}</p>
         </div>
-        <p className={style.title}>{randomPrize}등 당첨!</p>
+        <p className={style.title}>{randomPrize.current}등 당첨!</p>
         <div className={style.prize}></div>
         <p className={style.info}>{'[경품 이름]\n두줄인경우'}</p>
+        <p className={style.detail}>
+          {'당첨 안내 문자를 받을 연락처를 확인해주세요.\n(본인인증 완료한 연락처로만 가능합니다.)'}
+        </p>
 
-        <p>{'당첨 안내 문자를 받을 연락처를 확인해주세요.\n(본인인증 완료한 연락처로만 가능합니다.)'}</p>
+        <div className={style.phoneInfo}>
+          <p>연락처</p>
+          <p>인증된 연락처가 변경된 경우 본인인증 후 변경이 가능해요.</p>
+        </div>
 
-        <div>연락처... (진행중)</div>
+        <div className={style.phone}>
+          <p className={style.number}>010-0000-0000</p>
+          <div className={style.changeButton} onClick={() => openToast('PASS 본인인증 이동')}>
+            <p>변경하기</p>
+          </div>
+        </div>
       </div>
 
       <button onClick={() => router.push('/recreation/luckydraw')} className={style.confirmButton}>
