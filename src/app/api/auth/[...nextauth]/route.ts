1af44401670937797565539e17f9a7e6b397eeb9
jungwoole91 <jungwoole91@gmail.com>
2024-08-26 00:56:51 +0900

    [feat] next-auth 다운그레이드

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,126 @@
-export { GET, POST } from '../../../../auth';
+import NextAuth from 'next-auth';
+import google from 'next-auth/providers/google';
+import kakao from 'next-auth/providers/kakao';
+import apple from 'next-auth/providers/apple';
+
+const handler = NextAuth({
+  debug: true,
+  /** apple 로그인시 쿠키 설정 해줘야함 **/
+  cookies: {
+    pkceCodeVerifier: {
+      name: 'next-auth.pkce.code_verifier',
+      options: {
+        httpOnly: true,
+        sameSite: 'none',
+        path: '/',
+        secure: true,
+      },
+    },
+    callbackUrl: {
+      name: `__Secure-next-auth.callback-url`,
+      options: {
+        httpOnly: false,
+        sameSite: 'none',
+        path: '/',
+        secure: true,
+      },
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    google({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+    kakao({
+      clientId: process.env.KAKAO_CLIENT_ID ?? '',
+      clientSecret: process.env.KAKAO_CLIENT_SECRET ?? '',
+    }),
+
+    apple({
+      clientId: process.env.APPLE_CLIENT_ID ?? '',
+      clientSecret: process.env.APPLE_CLIENT_SECRET ?? '',
+      profile(profile) {
+        console.log(profile);
+        return {
+          id: profile.sub,
+          name: 'Person Doe',
+          email: profile.email,
+          image: '',
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user, account, profile }) {
+      console.log(account);
+      if (account) {
+        if (account.provider === 'google') {
+          const response = await fetch(`${process.env.API_URL}/auth/signin`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ accessToken: account.id_token, vendor: account.provider }),
+          });
+          console.log(`구글 로그인 토큰: ${account.id_token}`);
+
+          const result = await response.json();
+          token.accessToken = result.data.credentials.accessToken;
+          token.refreshToken = result.data.credentials.refreshToken;
+          token.accountId = result.data.accountId;
+          token.type = result.data.type;
+          token.accountId = result.data.accountId;
+          token.profileImg = profile?.picture;
+        } else if (account.provider === 'kakao') {
+          const response = await fetch(`${process.env.API_URL}/auth/signin`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ accessToken: account.access_token, vendor: account.provider }),
+          });
+
+          console.log(`카카오 로그인 토큰: ${account.access_token}`);
+          const result = await response.json();
+
+          token.accessToken = result.data.credentials.accessToken;
+          token.refreshToken = result.data.credentials.refreshToken;
+          token.type = result.data.type;
+          token.accountId = result.data.accountId;
+          // @ts-ignore
+          token.profileImg = profile?.properties?.profile_image;
+        } else if (account.provider === 'apple') {
+          console.log(`로그인 넘어옴??`);
+          console.log('Test 2');
+          console.log(account);
+          const response = await fetch(`${process.env.API_URL}/auth/signin`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ accessToken: account.id_token, vendor: account.provider }),
+          });
+
+          const result = await response.json();
+          token.accessToken = result.data.credentials.accessToken;
+          token.refreshToken = result.data.credentials.refreshToken;
+          token.type = result.data.type;
+          token.accountId = result.data.accountId;
+          // @ts-ignore
+          token.profileImg = profile?.properties?.profile_image;
+        }
+      }
+
+      return token;
+    },
+
+    async session({ session, token, trigger, newSession }) {
+      if (session) {
+        session.accessToken = token.accessToken + '';
+        session.refreshToken = token.refreshToken + '';
+        session.type = token.type + '';
+        session.accountId = token.accountId + '';
+        session.profileImg = token.profileImg + '';
+      }
+
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
--- src/middleware.ts
+++
@@ -1,6 +0,0 @@
-export { auth as middleware } from './auth';
-
-/** middleware를 적용할 route 경로 => 로그인을 해야만 접근가능한 페이지 **/
-export const config = {
-  matcher: [],
-};
