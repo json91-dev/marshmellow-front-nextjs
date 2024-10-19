98e96ffc19c17a4513977c1af8aba7e9dbd90d3f
jungwoole91 <jungwoole91@gmail.com>
2024-10-19 11:43:24 +0900

    [fix] 모달 누락

--- src/app/@modal/login/page.tsx
+++ src/app/@modal/login/page.tsx
@@ -3,5 +3,9 @@ import React from 'react';
 import RestoreWelcomeModal from '@/app/@modal/login/_components/RestoreWelcomeModal';
 
 export default function LoginPageModals() {
-  return <>{/*<RestoreWelcomeModal />*/}</>;
+  return (
+    <>
+      <RestoreWelcomeModal />
+    </>
+  );
 }
